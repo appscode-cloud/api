005acf563f9b45c5d7b591ccfb6bedf8a329ab1d
sauman <sauman@thetigerworks.com>
2016-04-01 18:02:54 +0600

    fix

--- pv/v0.1/pvc.pb.gw.go
+++ pv/v0.1/pvc.pb.gw.go
@@ -98,10 +98,6 @@ func request_PersistentVolumeClaims_Register_0(ctx context.Context, client Persi
 
 }
 
-var (
-	filter_PersistentVolumeClaims_Unregister_0 = &utilities.DoubleArray{Encoding: map[string]int{"cluster": 0, "name": 1}, Base: []int{1, 1, 2, 0, 0}, Check: []int{0, 1, 1, 2, 3}}
-)
-
 func request_PersistentVolumeClaims_Unregister_0(ctx context.Context, client PersistentVolumeClaimsClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq PVCUnregisterRequest
 	var metadata runtime.ServerMetadata
@@ -135,8 +131,15 @@ func request_PersistentVolumeClaims_Unregister_0(ctx context.Context, client Per
 		return nil, metadata, err
 	}
 
-	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_PersistentVolumeClaims_Unregister_0); err != nil {
-		return nil, metadata, grpc.Errorf(codes.InvalidArgument, "%v", err)
+	val, ok = pathParams["namespace"]
+	if !ok {
+		return nil, metadata, grpc.Errorf(codes.InvalidArgument, "missing parameter %s", "namespace")
+	}
+
+	protoReq.Namespace, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, err
 	}
 
 	msg, err := client.Unregister(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
@@ -242,7 +245,7 @@ var (
 
 	pattern_PersistentVolumeClaims_Register_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4, 1, 0, 4, 1, 5, 5}, []string{"api", "pv", "v0.1", "pvc", "cluster", "name"}, ""))
 
-	pattern_PersistentVolumeClaims_Unregister_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4, 1, 0, 4, 1, 5, 5}, []string{"api", "pv", "v0.1", "pvc", "cluster", "name"}, ""))
+	pattern_PersistentVolumeClaims_Unregister_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4, 1, 0, 4, 1, 5, 5, 1, 0, 4, 1, 5, 6}, []string{"api", "pv", "v0.1", "pvc", "cluster", "name", "namespace"}, ""))
 )
 
 var (
