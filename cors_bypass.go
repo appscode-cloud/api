e97a412285150ed495ef970ca31f032a6fcf663d
Tamal Saha <admin@thetigerworks.com>
2016-04-22 03:04:57 +0600

    added-all

--- cors_bypass.go
+++ cors_bypass.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	alert "github.com/appscode/api/alert/v0.1"
+	bucket "github.com/appscode/api/bucket/v0.1"
 	ci "github.com/appscode/api/ci/v0.1"
 	credential "github.com/appscode/api/credential/v0.1"
 	db "github.com/appscode/api/db/v0.1"
@@ -9,7 +10,6 @@ import (
 	kubernetes "github.com/appscode/api/kubernetes/v0.1"
 	namespace "github.com/appscode/api/namespace/v0.1"
 	pv "github.com/appscode/api/pv/v0.1"
-	bucket "github.com/appscode/api/bucket/v0.1"
 	"github.com/gengo/grpc-gateway/runtime"
 )
 
