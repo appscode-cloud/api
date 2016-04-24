d401deaae544177a7bb22414e870c40f4901ea46
Tamal Saha <admin@thetigerworks.com>
2016-04-24 20:52:36 +0600

    added-all

--- certificate/v0.1/freessl_certificate.schema.go
+++ certificate/v0.1/freessl_certificate.schema.go
@@ -8,40 +8,19 @@ import (
 // Auto-generated. DO NOT EDIT.
 
 var freeSSLCertificateRevokeRequestSchema *gojsonschema.Schema
-var freeSSLCertificateDeployRequestSchema *gojsonschema.Schema
 var freeSSLCertificateRegisterRequestSchema *gojsonschema.Schema
-var freeSSLCertificateCreateRequestSchema *gojsonschema.Schema
-var freeSSLCertificateDeleteRequestSchema *gojsonschema.Schema
-var freeSSLCertificateDescribeRequestSchema *gojsonschema.Schema
 var freeSSLCertificateRenewRequestSchema *gojsonschema.Schema
+var freeSSLCertificateCreateRequestSchema *gojsonschema.Schema
 
 func init() {
 	var err error
 	freeSSLCertificateRevokeRequestSchema, err = gojsonschema.NewSchema(gojsonschema.NewStringLoader(`{
   "$schema": "http://json-schema.org/draft-04/schema#",
   "properties": {
-    "cert": {
-      "type": "string"
-    }
-  },
-  "type": "object"
-}`))
-	if err != nil {
-		log.Fatal(err)
-	}
-	freeSSLCertificateDeployRequestSchema, err = gojsonschema.NewSchema(gojsonschema.NewStringLoader(`{
-  "$schema": "http://json-schema.org/draft-04/schema#",
-  "properties": {
-    "cert": {
+    "account_phid": {
       "type": "string"
     },
-    "cluster_name": {
-      "type": "string"
-    },
-    "namespace": {
-      "type": "string"
-    },
-    "secret_name": {
+    "cert_phid": {
       "type": "string"
     }
   },
@@ -52,45 +31,8 @@ func init() {
 	}
 	freeSSLCertificateRegisterRequestSchema, err = gojsonschema.NewSchema(gojsonschema.NewStringLoader(`{
   "$schema": "http://json-schema.org/draft-04/schema#",
-  "type": "object"
-}`))
-	if err != nil {
-		log.Fatal(err)
-	}
-	freeSSLCertificateCreateRequestSchema, err = gojsonschema.NewSchema(gojsonschema.NewStringLoader(`{
-  "$schema": "http://json-schema.org/draft-04/schema#",
   "properties": {
-    "cert_data": {
-      "type": "string"
-    },
-    "key_data": {
-      "type": "string"
-    },
-    "name": {
-      "type": "string"
-    }
-  },
-  "type": "object"
-}`))
-	if err != nil {
-		log.Fatal(err)
-	}
-	freeSSLCertificateDeleteRequestSchema, err = gojsonschema.NewSchema(gojsonschema.NewStringLoader(`{
-  "$schema": "http://json-schema.org/draft-04/schema#",
-  "properties": {
-    "cert": {
-      "type": "string"
-    }
-  },
-  "type": "object"
-}`))
-	if err != nil {
-		log.Fatal(err)
-	}
-	freeSSLCertificateDescribeRequestSchema, err = gojsonschema.NewSchema(gojsonschema.NewStringLoader(`{
-  "$schema": "http://json-schema.org/draft-04/schema#",
-  "properties": {
-    "cert": {
+    "email": {
       "type": "string"
     }
   },
@@ -102,8 +44,44 @@ func init() {
 	freeSSLCertificateRenewRequestSchema, err = gojsonschema.NewSchema(gojsonschema.NewStringLoader(`{
   "$schema": "http://json-schema.org/draft-04/schema#",
   "properties": {
-    "cert": {
+    "account_phid": {
       "type": "string"
+    },
+    "cert_phid": {
+      "type": "string"
+    },
+    "name": {
+      "type": "string"
+    }
+  },
+  "type": "object"
+}`))
+	if err != nil {
+		log.Fatal(err)
+	}
+	freeSSLCertificateCreateRequestSchema, err = gojsonschema.NewSchema(gojsonschema.NewStringLoader(`{
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "properties": {
+    "account_phid": {
+      "type": "string"
+    },
+    "bundle": {
+      "type": "boolean"
+    },
+    "common_name": {
+      "type": "string"
+    },
+    "key_data": {
+      "type": "string"
+    },
+    "name": {
+      "type": "string"
+    },
+    "san": {
+      "items": {
+        "type": "string"
+      },
+      "type": "array"
     }
   },
   "type": "object"
@@ -118,33 +96,18 @@ func (m *FreeSSLCertificateRevokeRequest) IsValid() (*gojsonschema.Result, error
 }
 func (m *FreeSSLCertificateRevokeRequest) IsRequest() {}
 
-func (m *FreeSSLCertificateDeployRequest) IsValid() (*gojsonschema.Result, error) {
-	return freeSSLCertificateDeployRequestSchema.Validate(gojsonschema.NewGoLoader(m))
-}
-func (m *FreeSSLCertificateDeployRequest) IsRequest() {}
-
 func (m *FreeSSLCertificateRegisterRequest) IsValid() (*gojsonschema.Result, error) {
 	return freeSSLCertificateRegisterRequestSchema.Validate(gojsonschema.NewGoLoader(m))
 }
 func (m *FreeSSLCertificateRegisterRequest) IsRequest() {}
 
-func (m *FreeSSLCertificateCreateRequest) IsValid() (*gojsonschema.Result, error) {
-	return freeSSLCertificateCreateRequestSchema.Validate(gojsonschema.NewGoLoader(m))
-}
-func (m *FreeSSLCertificateCreateRequest) IsRequest() {}
-
-func (m *FreeSSLCertificateDeleteRequest) IsValid() (*gojsonschema.Result, error) {
-	return freeSSLCertificateDeleteRequestSchema.Validate(gojsonschema.NewGoLoader(m))
-}
-func (m *FreeSSLCertificateDeleteRequest) IsRequest() {}
-
-func (m *FreeSSLCertificateDescribeRequest) IsValid() (*gojsonschema.Result, error) {
-	return freeSSLCertificateDescribeRequestSchema.Validate(gojsonschema.NewGoLoader(m))
-}
-func (m *FreeSSLCertificateDescribeRequest) IsRequest() {}
-
 func (m *FreeSSLCertificateRenewRequest) IsValid() (*gojsonschema.Result, error) {
 	return freeSSLCertificateRenewRequestSchema.Validate(gojsonschema.NewGoLoader(m))
 }
 func (m *FreeSSLCertificateRenewRequest) IsRequest() {}
+
+func (m *FreeSSLCertificateCreateRequest) IsValid() (*gojsonschema.Result, error) {
+	return freeSSLCertificateCreateRequestSchema.Validate(gojsonschema.NewGoLoader(m))
+}
+func (m *FreeSSLCertificateCreateRequest) IsRequest() {}
 
