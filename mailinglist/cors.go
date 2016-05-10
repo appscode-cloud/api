688371419f9916d36acecb05367eb240bb75d17f
Tamal Saha <tamal@appscode.com>
2016-05-10 13:59:32 +0600

    added-all

--- mailinglist/cors.go
+++ mailinglist/cors.go
@@ -5,5 +5,6 @@ import "github.com/gengo/grpc-gateway/runtime"
 func Patterns() []runtime.Pattern {
 	return []runtime.Pattern{
 		pattern_MailingList_Subscribe_0,
+		pattern_MailingList_Unsubscribe_0,
 	}
 }
