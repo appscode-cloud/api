18a8f15c1eb6fb471f4e6a339d791d045ec0c770
Shahriar <shahriar@thetigerworks.com>
2016-05-16 18:40:55 +0600

    Merge branch 'master' of bitbucket.org:thetigerworks/api
    # Conflicts:
    #	alert/v0.1/alert.pb.go
    #	artifactory/v0.1/artifactory.pb.go
    #	authentication/v0.1/authentication.pb.go
    #	billing/v0.1/charge.pb.go
    #	billing/v0.1/subscription.pb.go
    #	bucket/v0.1/bucket.pb.go
    #	certificate/v0.1/acme_user.pb.go
    #	certificate/v0.1/ca.pb.go
    #	certificate/v0.1/certificate.pb.go
    #	ci/v0.1/build.pb.go
    #	ci/v0.1/job.pb.go
    #	ci/v0.1/master.pb.go
    #	ci/v0.1/slave.pb.go
    #	credential/v0.1/cloud.pb.go
    #	data/files/data.go
    #	db/v0.1/db.pb.go
    #	db/v0.1/slaves.pb.go
    #	dtypes/types.pb.go
    #	gearman/gearman.pb.go
    #	glusterfs/v0.1/cluster.pb.go
    #	glusterfs/v0.1/volume.pb.go
    #	health/health.pb.go
    #	kubernetes/v0.1/client.pb.go
    #	kubernetes/v0.1/clusters.pb.go
    #	kubernetes/v0.1/events.pb.go
    #	loadbalancer/v0.1/loadbalancer.pb.go
    #	mailinglist/mailinglist.pb.go
    #	monitoring/v0.1/dashboard.pb.go
    #	namespace/v0.1/namespace.pb.go
    #	pv/v0.1/disk.pb.go
    #	pv/v0.1/pv.pb.go
    #	pv/v0.1/pvc.pb.go
    #	ssh/v0.1/ssh.pb.go

---
+++ glusterfs/v0.1/cluster.schema.go
@@ -0,0 +1,116 @@
+package glusterfs
+
+import (
+	"github.com/xeipuuv/gojsonschema"
+	"log"
+)
+
+// Auto-generated. DO NOT EDIT.
+
+var clusterDescribeRequestSchema *gojsonschema.Schema
+var clusterListRequestSchema *gojsonschema.Schema
+var clusterDeleteRequestSchema *gojsonschema.Schema
+var clusterCreateRequestSchema *gojsonschema.Schema
+
+func init() {
+	var err error
+	clusterDescribeRequestSchema, err = gojsonschema.NewSchema(gojsonschema.NewStringLoader(`{
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "properties": {
+    "kube_cluster": {
+      "type": "string"
+    },
+    "name": {
+      "type": "string"
+    }
+  },
+  "type": "object"
+}`))
+	if err != nil {
+		log.Fatal(err)
+	}
+	clusterListRequestSchema, err = gojsonschema.NewSchema(gojsonschema.NewStringLoader(`{
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "properties": {
+    "kube_cluster": {
+      "type": "string"
+    }
+  },
+  "type": "object"
+}`))
+	if err != nil {
+		log.Fatal(err)
+	}
+	clusterDeleteRequestSchema, err = gojsonschema.NewSchema(gojsonschema.NewStringLoader(`{
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "properties": {
+    "kube_cluster": {
+      "type": "string"
+    },
+    "name": {
+      "type": "string"
+    }
+  },
+  "type": "object"
+}`))
+	if err != nil {
+		log.Fatal(err)
+	}
+	clusterCreateRequestSchema, err = gojsonschema.NewSchema(gojsonschema.NewStringLoader(`{
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "properties": {
+    "bucket": {
+      "type": "string"
+    },
+    "credential": {
+      "type": "string"
+    },
+    "disks": {
+      "items": {
+        "type": "string"
+      },
+      "type": "array"
+    },
+    "enable_bacula": {
+      "type": "integer"
+    },
+    "kube_cluster": {
+      "type": "string"
+    },
+    "mood": {
+      "type": "string"
+    },
+    "name": {
+      "type": "string"
+    },
+    "node": {
+      "type": "integer"
+    }
+  },
+  "type": "object"
+}`))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (m *ClusterDescribeRequest) IsValid() (*gojsonschema.Result, error) {
+	return clusterDescribeRequestSchema.Validate(gojsonschema.NewGoLoader(m))
+}
+func (m *ClusterDescribeRequest) IsRequest() {}
+
+func (m *ClusterListRequest) IsValid() (*gojsonschema.Result, error) {
+	return clusterListRequestSchema.Validate(gojsonschema.NewGoLoader(m))
+}
+func (m *ClusterListRequest) IsRequest() {}
+
+func (m *ClusterDeleteRequest) IsValid() (*gojsonschema.Result, error) {
+	return clusterDeleteRequestSchema.Validate(gojsonschema.NewGoLoader(m))
+}
+func (m *ClusterDeleteRequest) IsRequest() {}
+
+func (m *ClusterCreateRequest) IsValid() (*gojsonschema.Result, error) {
+	return clusterCreateRequestSchema.Validate(gojsonschema.NewGoLoader(m))
+}
+func (m *ClusterCreateRequest) IsRequest() {}
+
