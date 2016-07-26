827375dc9fad67368ec3bc3402d2dd169ee6e51a
Md.Abdulla-Al-Sun <sun@thetigerworks.com>
2016-07-26 12:21:05 +0600

    added name with artifact listing

--- meta/data.go
+++ meta/data.go
@@ -83,7 +83,7 @@ func metaConfigYaml() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "meta/config.yaml", size: 883, mode: os.FileMode(436), modTime: time.Unix(1469073705, 0)}
+	info := bindataFileInfo{name: "meta/config.yaml", size: 883, mode: os.FileMode(436), modTime: time.Unix(1469179916, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
